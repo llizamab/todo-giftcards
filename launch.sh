d2a4c63fa5d2d216b596ab0af88253837ac45de0
root <root@luis-VirtualBox>
2018-03-17 01:17:43 -0300

    avance docker config dev

--- launch.sh
+++ launch.sh
@@ -4,7 +4,14 @@
 mvn clean install
 
 # compilo la imagen
-docker build -t giftcards-frontend ./giftcards-frontend
+docker build -f "./giftcards-frontend/DockerfileDev" -t giftcards-frontend ./giftcards-frontend
 
 # levanto el compose
 docker-compose -d up
+
+# ejecuto la carga de la bbdd
+./giftcards-bbdd/mvn flyway:clean flyway:migrate
+
+# levanto el backend
+./giftcards-services/mvn spring-boot:run
+
